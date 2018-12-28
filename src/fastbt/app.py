61a2de9a102684fd1d93f3d29dfe0dccd111485b
machine <uberdeveloper001@gmail.com>
2018-12-28 18:17:55 +0530

    Backtest web interface formula added

---
+++ src/fastbt/app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request, jsonify
+import sys
+import os
+from fastbt.rapid import *
+
+
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(
+        dict(
+            block_start_string="<%",
+            block_end_string="%>",
+            variable_start_string="%%",
+            variable_end_string="%%",
+            comment_start_string="<#",
+            comment_end_string="#>",
+        )
+    )
+
+
+app = CustomFlask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    col_types = ['lag', 'percent_change', 'rolling', 'formula', 'indicator']
+    context = {"name": "Ram", 'col_types': col_types}
+    return render_template("backtest.html", **context)
+
+
+@app.route("/")
+def bt():
+    # Run a backtest
+    return 'Hello World'
+
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    # Test function
+    if request.method == "POST":
+        return str(request.form)
+
+
+@app.route("/backtest", methods=["POST"])
+def run_backtest():
+    if request.method == "POST":
+        pass
+        # return str(request.form)
+    df = pd.read_csv('/home/machine/Projects/finance/nifty50',
+                     parse_dates=['timestamp'])
+    import json
+    columns = json.loads(request.form.get('columns'))
+    print(columns)
+    result = backtest(data=df, order='S', stop_loss=3, columns=columns)
+    return str(result.net_profit.sum())
+
+
+@app.route("/ds", methods=["GET", "POST"])
+def ds():
+    controls = [
+        {
+            "input": True,
+            "type": "file",
+            "name": "directory",
+            "placeholder": "Select a directory with a file",
+        },
+        {
+            "input": True,
+            "type": "text",
+            "name": "engine",
+            "placeholder": "sqlalchemy connection string or HDF5 filename",
+        },
+        {
+            "input": True,
+            "type": "text",
+            "name": "tablename",
+            "placeholder": "tablename in SQL or HDF",
+        },
+        {"select": True, "name": "mode", "choices": ["SQL", "HDF"]},
+    ]
+    context = {"controls": controls}
+    if request.method == "GET":
+        return render_template("datastore.html", **context)
+    elif request.method == "POST":
+        print(request.form)
+        return str(request.form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/fastbt/static/script.js
+++ src/fastbt/static/script.js
@@ -1,54 +1,74 @@
 console.log("Hello world");
 
 var app = new Vue({
-  el: "#app",
-  data: {
-    start: "2018-06-01",
-    end: "2018-06-30",
-    capital: 100000,
-    leverage: 1,
-    commission: 0,
-    slippage: 0,
-    selector: null,
-    columns_list: [
-      'open', 'high', 'low', 'close', 'volume'
-    ],
-    indicator_list: [
-      'SMA', 'EMA'
-
-    ],
-    columns: [],
-    temp_column: '',
-    conditions: [],
-    temp: null,
-    col: null,
-    param1: 0,
-    col_name: null
-  },
-  methods: {
-    clickMe(text) {
-      alert(text);
+    el: "#app",
+    data: {
+        columns_list: ["open", "high", "low", "close", "volume"],
+        operators: ["+", "-"],
+        indicator_list: ["SMA", "EMA"],
+        col_type: "lag",
+        col_name: null,
+        columns: [],
+        conditions: [],
+        formula: null,
+        isIndicator: false,
+        isFormula: false,
+        isRolling: false
     },
-    addToText(text) {
-      this.temp_column = this.temp_column + text
-    },
-    addToList() {
-      this.collect.push({
-        num: this.temp
-      });
-      this.result = JSON.stringify(this.collect);
-      console.log("LOGGED", JSON.stringify(this.collect));
-      this.temp = null;
-    },
-    addColumn() {
-      this.columns.push(
-        JSON.stringify({
-          func: this.col,
-          param: this.param1,
-          col_name: this.col_name
-        })
-      );
-      this.col = this.param1 = this.col_name = null;
+    methods: {
+        setStatus(text) {
+            // Set the status of the variable
+            switch (text) {
+                case "indicator":
+                    this.isIndicator = true;
+                    this.isFormula = false;
+                    this.isRolling = false;
+                    break;
+                case "rolling":
+                    this.isRolling = true;
+                    this.isFormula = false;
+                    this.isIndicator = false;
+                    break;
+                case "formula":
+                    this.isFormula = true;
+                    this.isRolling = false;
+                    this.isIndicator = false;
+                    break;
+            }
+        },
+        evalFormula() {
+            // evaluate formula
+            if (this.col_name == null) {
+                return false;
+            }
+            if (this.formula == null) {
+                return false;
+            }
+            return {
+                F: {
+                    formula: this.formula,
+                    col_name: this.col_name
+                }
+            };
+        },
+        addColumn(text) {
+            let mapper = {
+                lag: "L",
+                percent_change: "P",
+                formula: "F",
+                indicator: "I",
+                rolling: "R"
+            };
+            let col_type = mapper[text];
+            if (col_type == "F") {
+                let val = this.evalFormula();
+                if (val) {
+                    console.log(val);
+                    this.columns.push(val);
+                }
+                this.formula = null;
+                this.col_name = null;
+            }
+        }
     }
-  }
 });
