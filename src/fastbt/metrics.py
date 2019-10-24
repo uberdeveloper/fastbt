624e753e5fd7462cddd20486cbc3e02e1a4d8a45
machine <uberdeveloper001@gmail.com>
2019-10-24 21:33:11 +0530

    spread test added

--- src/fastbt/metrics.py
+++ src/fastbt/metrics.py
@@ -9,14 +9,26 @@ try:
 except ImportError:
 	print('pyfolio not installed')
 
-def spread_test(data, periods=None):
+def spread_test(data, periods=['Y','Q','M']):
 	"""
 	Test whether the returns are spread over the entire period
 	or consolidated in a single period
 	data
 		returns/pnl as series with date as index
 	periods
-		periods to check
+		periods to check as list.
+		all valid pandas date offset strings accepted
+
+	returns a dataframe with periods as index and 
+	profit/loss count and total payoff
 	"""
-	pass
+	collect = []
+	for period in periods:
+		rsp = data.resample(period).sum()
+		gt = rsp[rsp >= 0]
+		lt = rsp[rsp < 0]
+		values = (len(gt), gt.sum(), len(lt), lt.sum())
+		collect.append(values)
+	return pd.DataFrame(collect, index=periods, 
+		columns=['num_profit', 'profit', 'num_loss', 'loss'])
 	
