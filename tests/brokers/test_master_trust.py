c24185e0f991c54a1a1091c773387e82e9b927be
uberdeveloper <uberdeveloper001@gmail.com>
2020-12-28 11:54:09 +0530

    initial tests for master trust broker added

---
+++ tests/brokers/test_master_trust.py
@@ -0,0 +1,51 @@
+import pytest
+from unittest.mock import Mock,patch,call
+
+from fastbt.brokers.master_trust import *
+import requests
+import json
+
+contracts_data = {
+        'NSE': [
+            {"trading_symbol":"SBIN-EQ","symbol":"SBIN","exchange_code":1,"exchange":"NSE","company":"STATE BANK OF INDIA","code":"3045"},
+            {"trading_symbol":"SBT-EQ","symbol":"SBT","exchange_code":1,"exchange":"NSE","company":"STATE BANK OF TRAVANCORE","code":"5316"}],
+        'NSE-OTH': 
+        [{"trading_symbol":"1018GS2026-GS","symbol":"1018GS2026 GS","exchange_code":1,"exchange":"NSE","company":"GOI LOAN 10.18% 2026","code":"6833"}]
+        }
+
+
+@patch('requests.get')
+def test_fetch_all_contracts(mock_get):
+    mock_get.return_value.json.return_value = contracts_data
+    result = {
+            'NSE:SBIN-EQ': '3045',
+            'NSE:SBT-EQ': '5316',
+            'NSE:1018GS2026-GS': '6833'
+            }
+    contracts = fetch_all_contracts(exchanges=['NSE'])
+    assert result == contracts
+
+
+@patch('requests.get')
+def test_fetch_all_contracts_multiple_exchanges(mock_get):
+    mock_get.return_value.json.return_value = contracts_data
+    result = {
+            'NSE:SBIN-EQ': '3045',
+            'NSE:SBT-EQ': '5316',
+            'NSE:1018GS2026-GS': '6833',
+            'NFO:SBIN-EQ': '3045',
+            'NFO:SBT-EQ': '5316',
+            'NFO:1018GS2026-GS': '6833'
+            }
+    contracts = fetch_all_contracts()
+    assert result == contracts
+
+@patch('requests.get')
+def test_get_instrument_token(mock_get):
+    mock_get.return_value.json.return_value = contracts_data
+    contracts = fetch_all_contracts()
+    token = get_instrument_token(contracts, 'NSE', 'SBIN-EQ')
+    assert token == '3045'
+    token = get_instrument_token(contracts, 'NFO', 'SBT-EQ')
+    assert token == '5316'
+
