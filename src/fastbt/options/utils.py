e50ec0782c68e2c70109d4571615595f44166316
uberdeveloper <uberdeveloper001@gmail.com>
2023-03-08 19:21:25 +0530

    monthly expiry added

--- src/fastbt/options/utils.py
+++ src/fastbt/options/utils.py
@@ -6,9 +6,12 @@ get_itm = utils.get_itm
 get_atm = utils.get_atm
 
 
-def get_expiry(expiries: List[pendulum.DateTime], n: int = 0, sort: bool = True):
+def get_expiry(
+    expiries: List[pendulum.DateTime], n: int = 1, sort: bool = True
+) -> pendulum.DateTime:
     """
     get the nth expiry from the list of expiries
+    **1 is the current expiry**
     expiries
         list of sorted expiries
     n
@@ -16,8 +19,41 @@ def get_expiry(expiries: List[pendulum.DateTime], n: int = 0, sort: bool = True)
         this is just a simple python list index
     sorted
         if True, the list is sorted and then values are returned. If you already have a sorted list, you can pass False to save on time
+    Note
+    ----
+    1) expiries start at 1 (not 0)
     """
+    n = n if n < 1 else n - 1
     if sort:
         return sorted(expiries)[n]
     else:
         return expiries[n]
+
+
+def get_monthly_expiry(
+    expiries: List[pendulum.DateTime], n: int = 1, sort: bool = True
+) -> pendulum.DateTime:
+    """
+    get the nth monthly expiry from the list of expiries
+    returns the last expiry in the month
+    expiries
+        list of sorted expiries
+    n
+        number of expiry to return
+        this is just a simple python list index
+    sorted
+        if True, the list is sorted and then values are returned.
+    Note
+    ----
+    1) expiries start at 1 (not 0)
+    2) the last expiry in the month is returned
+    """
+    if sort:
+        expiries = sorted(expiries)
+    i = 1
+    prev = expiries[0]
+    for prev, date in zip(expiries[:-1], expiries[1:]):
+        if prev.month != date.month:
+            i += 1
+            if i > n:
+                return prev
--- tests/options/test_option_utils.py
+++ tests/options/test_option_utils.py
@@ -13,6 +13,20 @@ def expiry_dates():
     return dates
 
 
+@pytest.fixture
+def expiry_dates2():
+    """
+    contains a lot of dates
+    """
+    start = pendulum.date(2021, 1, 1)
+    end = pendulum.date(2024, 8, 31)
+    period = pendulum.period(start, end)
+    dates = []
+    for p in period.range("weeks"):
+        dates.append(p)
+    return dates
+
+
 @pytest.mark.parametrize(
     "test_input, expected",
     [
@@ -38,7 +52,7 @@ def test_get_itm(test_input, expected):
 
 def test_get_expiry(expiry_dates):
     assert get_expiry(expiry_dates, sort=False) == pendulum.date(2021, 1, 31)
-    assert get_expiry(expiry_dates, 2, sort=False) == pendulum.date(2021, 3, 31)
+    assert get_expiry(expiry_dates, 3, sort=False) == pendulum.date(2021, 3, 31)
     assert get_expiry(expiry_dates, -1, sort=False) == pendulum.date(2022, 6, 30)
 
 
@@ -48,5 +62,12 @@ def test_get_expiry_unsorted(expiry_dates):
     # Make sure the first date is not the least expiry
     assert dates[0] != pendulum.date(2021, 1, 31)
     assert get_expiry(dates) == pendulum.date(2021, 1, 31)
-    assert get_expiry(dates, 2) == pendulum.date(2021, 3, 31)
+    assert get_expiry(dates, 2) == pendulum.date(2021, 2, 28)
     assert get_expiry(dates, -1) == pendulum.date(2022, 6, 30)
+
+
+def test_get_monthly_expiry(expiry_dates2):
+    dates = expiry_dates2
+    assert get_monthly_expiry(dates) == pendulum.date(2021, 1, 29)
+    assert get_monthly_expiry(dates, 5) == pendulum.date(2021, 5, 28)
+    assert get_monthly_expiry(dates, 27) == pendulum.date(2023, 3, 31)
